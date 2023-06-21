b13d1608a77451ad8128950f74813caee0fce8c6
sujal3011 <jaiswalsujal639@gmail.com>
2023-06-21 14:02:04 +0530

    minor changes

--- REChase/REChase/settings.py
+++ REChase/REChase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['rechase2023.pythonanywhere.com', '*']
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 # Application definition
 
--- REChase/teams/forms.py
+++ REChase/teams/forms.py
@@ -5,7 +5,6 @@ from .models import (
     Player
 )
 
-
 class TeamCreationForm(forms.ModelForm):
     class Meta:
         model = Team
